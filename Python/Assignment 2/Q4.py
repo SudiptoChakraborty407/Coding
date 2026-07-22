a1be4134acc68907805b5f810882827a1cb5aa93
Sudipto Chakraborty <sudipto.chakraborty407@gmail.com>
2026-07-22 08:13:45 +0530

    Added C and Python Folders

---
+++ Python/Assignment 2/Q4.py
@@ -0,0 +1,5 @@
+a = int(input("Enter the given number: "))
+if(a > 6):
+    print("The given number is greater than 6")
+else:
+    print("The given number is lesser than 6")
---
+++ Python/Assignment 2/Q6.py
@@ -0,0 +1,4 @@
+l = 14
+b = 5
+print("area = ", l*b)
+print("perimeter = ", 2*(l+b))
