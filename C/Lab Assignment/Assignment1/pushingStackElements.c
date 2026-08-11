42c0d317a3f98100e7e213456fef99d5b4aa6ba1
Sudipto Chakraborty <sudipto.chakraborty407@gmail.com>
2026-08-11 12:47:06 +0530

    Updating

---
+++ C/Lab Assignment/Assignment1/pushingStackElements.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX 100
+
+char stack1[MAX];
+int top = -1;
+
+void push(char n){
+    if (top<MAX-1){
+        top++;
+       stack1[top] = n;
+    }
+    else{
+        printf("Stack Overflow \n");
+    }
+}
+
+void display(){
+    if(top==-1){
+        printf("Stack is empty \n");
+    }
+    else{
+        printf("The elements of the stack are: \n");
+        for(int i=0;i<=top;i++){
+            printf("%d ", stack1[i]);
+        }
+        printf("\n");
+    }
+}
+
+int main(){
+    push (10);
+    push (20);
+    push (30);
+    push (40);
+    push (50);
+    display();
+    
+}
+
